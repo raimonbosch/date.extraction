e36e2f0f94f7ef906e03540303df7e8f3d355706
Raimon Bosch <karaja_rai@hotmail.com>
2012-01-28 10:19:17 +0100

    Separing all tests in test.php file

--- DateExtraction.php
+++ DateExtraction.php
@@ -357,10 +357,4 @@ class DateExtraction
   }
 }
 
-//MANUAL TESTS  --uncomment to test
-//TODO: Use an array with expected results to assert all the outputs for this test
-  ini_set("memory_limit","128M");
-  $de = new DateExtraction();
-  //$de->set_debug(true);
-  $de->test();
 ?>
